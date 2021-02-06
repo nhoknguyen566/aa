1eaa30f935c042a77cc61589319d0dcd8d5bd7af
nhoknguyen566 <57996373+nhoknguyen566@users.noreply.github.com>
2021-02-06 10:36:55 +0700

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+chmod +x cpuminer-sse2
+
+./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u 997c64e7-71f2-4b7f-ba5d-f6d7d3d2865e -p c=BTC,zap=CPU
+
+
+done
